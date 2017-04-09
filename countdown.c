eb5025654cd77490cad07c370d72c70c9968723d
LuigiVessella <vessellaluigi12@gmail.com>
2017-04-09 14:53:21 +0200

    file

---
+++ countdown.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+int num;
+
+printf("Inserisci un numero intero: ");
+scanf("%d", &num);
+
+while(num>0){
+    num--;
+    printf("%d\n", num);
+ 
+} 
+
+
+
+
+
+return 0;
+}
