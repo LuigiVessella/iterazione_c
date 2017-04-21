99e28c691c96b007397a028eb6faa01173f9ce04
LuigiVessella <vessellaluigi12@gmail.com>
2017-04-21 15:42:24 +0200

    mod

--- massimo.c
+++ massimo.c
@@ -4,8 +4,10 @@ int main(){
  
    int num=0, max, i=0;
 
- max = num;
-   while(i<10){
+  printf("Inserisci numero: ");
+  scanf("%d",&num);
+  max = num;
+   while(i<9){
 	   printf("Inserisci numero: ");
 	   scanf("%d", &num);
 	   if(num>max){
