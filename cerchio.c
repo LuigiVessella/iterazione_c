aa3888ba2c83400a7bc38b2f7284527ed1850587
LuigiVessella <vessellaluigi12@gmail.com>
2017-05-23 16:35:10 +0200

    file

--- cerchio.c
+++ cerchio.c
@@ -2,18 +2,20 @@
 
 int main(){
 
-	float raggio;
-        float circo=0, area=0;
-	do{
-		printf("Inserisci raggio: ");
-		scanf("%f", &raggio);
-                if(raggio<=0){
-		       printf("Non puoi inserire un numero <= di 0\n");
-		}	       
+float raggio;
+float circo=0.0, area=0.0;
+
+do{		
+	printf("Inserisci raggio: ");
+	scanf("%f", &raggio);
+         if(raggio<=0){
+	     printf("Non puoi inserire un numero <= di 0\n");
+         	}	       
 	}while(raggio<=0);
 
   area= raggio*raggio*3.14;
-  circo=2*raggio*3.14;
-  printf("Lunghezza circonferenza: %f\n Area: %f\n",circo, area);
-return 0;
+  circo=2.0*raggio*3.14;
+  printf("Lunghezza circonferenza: %.2f\nArea: %.2f\n",circo, area);
+  
+  return 0;
 }
