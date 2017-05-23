3aecb798e834d0b99052bc73bec500d321f8cd10
LuigiVessella <vessellaluigi12@gmail.com>
2017-05-23 19:47:42 +0200

    file

---
+++ occifra.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+ 
+ int num;
+ int i=0,a=0;
+ int modulo;
+
+ do{
+ printf("Inserisci un numero: ");
+ scanf("%d",&num); 
+ }while(num<0);
+ while(num%10 != 0){
+
+       modulo = num%10;
+       i = modulo;
+       if(i==7)
+	       a++;
+       num = num/10;
+ }
+ printf("%d\n", a);
+
+
+return 0;
+
+}
