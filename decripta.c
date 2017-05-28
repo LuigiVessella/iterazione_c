3d7012ab39be4aa05845c8f1a18fd84775a7363c
LuigiVessella <vessellaluigi12@gmail.com>
2017-05-28 13:51:05 +0200

    file

--- decripta.c
+++ decripta.c
@@ -4,29 +4,29 @@ int main(){
   int num;
   int a,b,c,d;
   int modulo; 
-  do{
+ 
  printf("Inserisci numero criptato con l'esercizio cripta.c: ");
  scanf("%d",&num);
-  }while(num<=999 || num>9999);
+ 
  
  modulo=num%10;
- d=(modulo-7)%10;
+ d=(modulo+3)%10;
 
   num/=10;
   modulo=num%10;
-  c=(modulo-7)%10;
+  c=(modulo+3)%10;
 
   num/=10;
   modulo=num%10; 
-  b=(modulo-7)%10;
+  b=(modulo+3)%10;
 
 
   num/=10;
   modulo=num%10;
-  a=(modulo-7)%10;
+  a=(modulo+3)%10;
 
 
-  printf("Il numero decriptato e': %d%d%d%d\n",a,b,c,d);
+  printf("Il numero decriptato e': %d%d%d%d\n",c,d,a,b);
 
 
 
