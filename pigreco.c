1d8ede41319ed22cce87ce2feeb9c5221293e4fb
LuigiVessella <vessellaluigi12@gmail.com>
2017-10-24 19:25:16 +0200

    modifiche

--- pigreco.c
+++ pigreco.c
@@ -2,17 +2,17 @@
 
 int main(){
  
- float pig1,tot=0;
+ float pig,tot=0;
  int num,i;
  float div= 3.0;
 
  printf("Quanti termini vuoi utilizzare: ");
  scanf("%d", &num);
 
- pig1=4.0-(4.0/div);
- tot=pig1;
+ pig=4.0-(4.0/div);
+ tot=pig;
 
- for(i=1; i<=num; i++){
+ for(i=1; i<=num-2; i++){
     div+=2.0;
   
     if(i%2 == 0)
