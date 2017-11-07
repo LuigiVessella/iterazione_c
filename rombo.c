86f9732820f244d042a706981547763e206980a1
LuigiVessella <vessellaluigi12@gmail.com>
2017-11-07 12:09:31 +0100

    file

--- rombo.c
+++ rombo.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
 int main(){
-int num,a,i;
+  int num=0, riga, colo=1, spazi=0;
 
-printf("Numero di righe dispari(1-19): ");
-scanf("%d", &num);
+  do{
+  printf("Inserisci numero di righe(dispari 1-19): ");
+  scanf("%d", &num);
+  }while(num%2==0 || num>19);
 
-for(i=0; i<=num; i++){
-   putchar(' ');
-   for(a
+  for(riga=1; colo<=num/2; colo++){
+    putchar(' ');
+  }
+   putchar('*');
+   
+   for(riga=1; colo<=num; colo++){
+     putchar(' ');
 }
-}
-
-}
-
+for(riga=2; riga<=num/2+1; riga++){
+  for(colo=0; colo<=riga; colo++){
+    putchar(' ');
+  }
+  putchar('*');
+  
 
 return 0;
 }
